ae4561f60a8089311f7f2bbc08d66bcae7c1db68
Denis Kibet <kibet1816@gmail.com>
2019-06-29 17:06:09 +0300

    Add create_app function in __init__.py

---
+++ app/main/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    """
+    Function that handles all app configs and initializing flask extensions
+
+    Args:
+        ConfigName:Configuration key setting
+    """
+    app = Flask(__name__)
+
+    # create app configurations
+    app.config.from_object(config_options[config_name])
+
+    # initializing flask extensions
+    bootstrap.init_app(app)
+
+    return app
