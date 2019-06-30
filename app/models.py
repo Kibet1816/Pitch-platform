b394489c0a817f57ff104c6e68c4a111f338fe62
Denis Kibet <kibet1816@gmail.com>
2019-06-30 14:26:28 +0300

    Add role model class in models.py

--- app/models.py
+++ app/models.py
@@ -11,6 +11,23 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255))
 
+class Role(db.Model):
+    """
+    Class that will define all different roles
+
+    Args:
+        DbModel:Connects our class to our database and allow communication
+    """
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(255))
+
+    def __repr__(self):
+        """
+        Function that makes debugging easier
+        """
+        return f'User {self.name}'
+
+
     def __repr__(self):
         """
         Function that makes debugging easier
