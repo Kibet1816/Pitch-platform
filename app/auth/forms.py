ae75e1311998b26944f34bfaaed993d2a570035c
Denis Kibet <kibet1816@gmail.com>
2019-06-30 21:04:16 +0300

    Add login form in forms.py

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -2,6 +2,19 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,ValidationError
 from wtforms.validators import Required,Email,EqualTo
 from ..models import User
+from wtforms import StringField,PasswordField,BooleanField,SubmitField
+
+class LoginForm(FlaskForm):
+    """
+    Class to give users access to some features of our app
+
+    Args:
+        FlaskForm:
+    """
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')
 
 class RegistratonForm(FlaskForm):
     """
