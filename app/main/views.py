e8ec05f11316ea743d251b6207ba8615c36a5e6f
Denis Kibet <kibet1816@gmail.com>
2019-06-30 19:48:29 +0300

    Add function to get the user id in models.py

--- app/main/views.py
+++ app/main/views.py
@@ -7,3 +7,8 @@ def index():
     View root page function that returns the index page
     """
     return render_template('index.html')
+
+@main.route('/pitches/review/new/<int:id>',methods = ['GET','POST'])
+@login_required
+def new_review():
+    pass
--- app/models.py
+++ app/models.py
@@ -1,7 +1,19 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
 
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    """
+    Function that queries the database and gets a user with the id
+
+    Args:
+        UserId:The id of the specific user
+    """
+    return User.query.get(int(user_id))
+
+class User(Usermixin,db.Model):
     """
     User model class
 
@@ -11,7 +23,8 @@ class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer,primary_key = True)
-    username = db.Column(db.String(255))
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
     pass_secure = db.Column(db.String(255))
 
