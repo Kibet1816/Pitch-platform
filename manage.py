d5218f16b1b5bc31d5ab34ab64db6b7296b43a4c
Denis Kibet <kibet1816@gmail.com>
2019-06-30 14:26:44 +0300

    Install psycopg2 without errors

--- manage.py
+++ manage.py
@@ -1,11 +1,19 @@
-from app import create_app
-from flask_script import Manager,Server
+from app import create_app,db
+from app.models import User
+from flask_script import Manager,Server,Shell
 
 # create app instance
 app = create_app('development')
 
 manager = Manager(app)
 manager.add_command('server',Server)
+
+@manager.shell
+def make_shell_context():
+    """
+    Function that allows passing properties into our shell
+    """
+    return dict(app = app,db = db,User = User)
 
 if __name__ == '__main__':
     manager.run()
