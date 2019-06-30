be516b877543f61a7c9180510d29847d84f2e0cf
Denis Kibet <kibet1816@gmail.com>
2019-06-30 15:47:21 +0300

    Register auth blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -26,6 +26,8 @@ def create_app(config_name):
 
     # registering the blueprint
     from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix='/authenticate')
     app.register_blueprint(main_blueprint)
 
     return app
