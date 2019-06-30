50ac415c3e427f54e8dee63006c508ac26cc2eb8
Denis Kibet <kibet1816@gmail.com>
2019-06-30 20:57:01 +0300

    Add register function in views.py

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,8 @@
-from flask import render_template
+from flask import render_template,redirect,url_for
 from . import auth
+from ..models import User
+from .forms import RegistratonForm
+from .. import db
 
 @auth.route('/login')
 def login():
@@ -7,3 +10,18 @@ def login():
     View function that renders the login page
     """
     return render_template('auth/login.html')
+
+@auth.route('/register',methods = ['GET','POST'])
+def register():
+    """
+    Function to register new users
+    """
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.add(user)
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
+
