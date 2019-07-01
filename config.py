cf6380d31bd4f8b66377ea0172c9310e01d225f2
Denis Kibet <kibet1816@gmail.com>
2019-07-01 07:49:13 +0300

    Add email configurations to config.py file

--- config.py
+++ config.py
@@ -4,6 +4,11 @@ class Config():
     SECRET_KEY = 'blaugrana'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://denis1816:kibet@localhost/forza'
     UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     pass
