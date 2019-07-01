7b8fe11d6ad77081e3f83c09ad60ee71899047a2
Denis Kibet <kibet1816@gmail.com>
2019-07-01 07:41:10 +0300

    Create new configuration in config.py

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 class Config():
     SECRET_KEY = 'blaugrana'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://denis1816:kibet@localhost/forza'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
 
 class ProdConfig(Config):
     pass
