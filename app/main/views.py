ade634b592a236ec804ba701b903cd1b27140f4f
Denis Kibet <kibet1816@gmail.com>
2019-07-01 07:31:22 +0300

    Add function to update profile in views.py

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,8 @@ from flask import render_template,request,redirect,url_for,abort
 from . import main
 from flask_login import login_required
 from ..models import User
+from .forms import PitchForm,UpdateProfile
+from .. import db
 
 @main.route('/')
 def index():
@@ -25,3 +27,29 @@ def profile(uname):
         abort(404)
 
     return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    """
+    View function to handle update profile request
+
+    Args:
+        Uname:The users username
+    """
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname = user.username))
+
+    return render_template('profile/update.html',form =form)
+
