0222abd4be67e84f7ecd850f62e0176ea83d08ee
Denis Kibet <kibet1816@gmail.com>
2019-07-01 07:51:36 +0300

    Import and initialize flask_mail in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,7 @@ from flask import Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -12,6 +13,7 @@ login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet()
+mail = Mail()
 
 def create_app(config_name):
     """
@@ -30,6 +32,7 @@ def create_app(config_name):
     db.init_app(app)
     login_manager.init_app(app)
     configure_uploads(app,photos)
+    mail.init_app(app)
 
     # registering the blueprint
     from .main import main as main_blueprint
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MAIL_USERNAME=kibet1816@gmail.com
+export MAIL_PASSWORD=soccer524
+
+python3.6 manage.py server
