74e40046701f5035436a6ad8d613695d551a2927
Denis Kibet <kibet1816@gmail.com>
2019-07-02 08:22:22 +0300

    Remove email configurations

--- config.py
+++ config.py
@@ -6,19 +6,14 @@ class Config():
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     
-    # email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
     
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-
+    # SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
+    
 class DevConfig(Config):
     pass
 
